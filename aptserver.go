e94e6ce85786a51e382825f25f228b6096548ce0
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2014-05-29 17:20:16 +0100

    More refactoring

--- aptserver.go
+++ aptserver.go
@@ -40,8 +40,8 @@ func (a *AptServer) InitAptServer() {
 
 	a.aptLock = make(chan int, 1)
 	a.aptLock <- 1
-  a.downloadHandler = makeDownloadHandler(a.getLocks, a.RepoDir)
-	a.uploadHandler = makeUploadHandler(a.TmpDir, a.CookieName, a.TTL)
+  a.downloadHandler = makeDownloadHandler(a)
+	a.uploadHandler = makeUploadHandler(a)
 }
 
 func (a *AptServer) Register(r *mux.Router) {
@@ -50,18 +50,61 @@ func (a *AptServer) Register(r *mux.Router) {
 	r.HandleFunc("/package/upload/{session}", a.uploadHandler).Methods("POST", "PUT")
 }
 
-func makeDownloadHandler(lockChan chan int, dir string) http.HandlerFunc {
+func makeDownloadHandler(a *AptServer) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		lock := <-lockChan
-		defer func() { lockChan <- lock }()
+		lock := <-a.getLocks
+		defer func() { a.getLocks <- lock }()
 
 		file := mux.Vars(r)["rest"]
-		realFile := dir + "/" + file
+		realFile := a.TmpDir + "/" + file
 
     log.Println("req'd " + realFile)
 
 		http.ServeFile(w, r, realFile)
 	}
+}
+
+func makeUploadHandler(a *AptServer) (f func(w http.ResponseWriter, r *http.Request)){
+  dispatch := make(chan *uploadReq)
+  complete := make(chan string)
+
+  go dispatcher(dispatch, complete)
+
+  f = func(w http.ResponseWriter, r *http.Request) {
+		// Did we get a session
+		session, found := mux.Vars(r)["session"]
+
+		//maybe in a cookie?
+		if !found {
+			cookie, err := r.Cookie(a.CookieName)
+			if err == nil {
+				session = cookie.Value
+			}
+		}
+
+		if session == "" {
+			session := uuid.New()
+			cookie := http.Cookie{
+				Name:     a.CookieName,
+				Value:    session,
+				Expires:  time.Now().Add(a.TTL),
+				HttpOnly: false,
+				Path:     "/package/upload"}
+			http.SetCookie(w, &cookie)
+			w.Write([]byte(uuid.New()))
+
+			dir := a.TmpDir + "/" + session
+			os.Mkdir(dir, os.FileMode(0755))
+
+			go pathHandle(dir, a.TTL)
+
+		} else {
+			w.Write([]byte("Hello3 " + session))
+      dispatch <- &uploadReq{session, w, r}
+		}
+	}
+
+  return
 }
 
 type uploadReq struct {
@@ -108,47 +151,3 @@ func dispatcher(reqs chan *uploadReq, done chan string) {
   }
 }
 
-func makeUploadHandler(
-	tmpDir string,
-	cookieName string,
-	expire time.Duration) func(http.ResponseWriter, *http.Request) {
-
-  dispatch := make(chan *uploadReq)
-  complete := make(chan string)
-
-  go dispatcher(dispatch, complete)
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Did we get a session
-		session, found := mux.Vars(r)["session"]
-
-		//maybe in a cookie?
-		if !found {
-			cookie, err := r.Cookie(cookieName)
-			if err == nil {
-				session = cookie.Value
-			}
-		}
-
-		if session == "" {
-			session := uuid.New()
-			cookie := http.Cookie{
-				Name:     cookieName,
-				Value:    session,
-				Expires:  time.Now().Add(expire),
-				HttpOnly: false,
-				Path:     "/package/upload"}
-			http.SetCookie(w, &cookie)
-			w.Write([]byte(uuid.New()))
-
-			dir := tmpDir + "/" + session
-			os.Mkdir(dir, os.FileMode(0755))
-
-			go pathHandle(dir, expire)
-
-		} else {
-			w.Write([]byte("Hello3 " + session))
-      dispatch <- &uploadReq{session, w, r}
-		}
-	}
-}
