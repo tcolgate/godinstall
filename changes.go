41164e117524c1ccab73dab6f8423ee98667e113
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2014-05-31 21:03:50 +0100

    Start refactor to make things less interdependent and more testable

--- changes.go
+++ changes.go
@@ -2,20 +2,64 @@ package main
 
 import (
 	//"code.google.com/p/go.crypto/openpgp"
-	//"code.google.com/p/go.crypto/openpgp/armor"
-	"github.com/stapelberg/godebiancontrol"
+	"bytes"
 	"io"
+	"io/ioutil"
+	"log"
+
+	"code.google.com/p/go.crypto/openpgp"
+	"code.google.com/p/go.crypto/openpgp/clearsign"
+	"github.com/stapelberg/godebiancontrol"
 )
 
 type DebChanges struct {
-	p []godebiancontrol.Paragraph
+	signed    bool
+	validated bool
+	signedBy  *openpgp.Entity
+	p         []godebiancontrol.Paragraph
 }
 
-func ParseDebianChanges(r io.Reader) (*DebChanges, error) {
-	var changes DebChanges
-	var err error
+func ParseDebianChanges(r io.Reader, kr *openpgp.KeyRing) (p *DebChanges, err error) {
+	var c DebChanges
 
-	changes.p, err = godebiancontrol.Parse(r)
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return
+	}
 
-	return &changes, err
+	msg, rest := clearsign.Decode(b)
+	switch {
+	case len(msg.Plaintext) == 0 && len(rest) > 0:
+		{
+			c.signed = false
+		}
+	case len(msg.Plaintext) > 0:
+		{
+			c.signed = true
+			if len(rest) > 0 {
+				log.Println("trailing content in signed control file will be ignored")
+			}
+		}
+	}
+
+	br := bytes.NewReader(rest)
+
+	if c.signed {
+		br = bytes.NewReader(msg.Bytes)
+
+		c.signedBy, err = openpgp.CheckDetachedSignature(*kr, br, msg.ArmoredSignature.Body)
+		if err == nil {
+			c.validated = true
+		} else {
+			c.validated = false
+		}
+		br = bytes.NewReader(msg.Plaintext)
+	} else {
+		c.validated = false
+	}
+
+	c.p, err = godebiancontrol.Parse(br)
+	// Need to check for parse failing here
+
+	return &c, err
 }
--- upload.go
+++ upload.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"net/http"
@@ -11,14 +8,28 @@ import (
 	"time"
 
 	"code.google.com/p/go.crypto/openpgp"
-	"code.google.com/p/go.crypto/openpgp/clearsign"
 )
 
 type uploadSession struct {
-	SessionId string // Name of the session
-	dir       string // Temporary directory for storage
-	keyRing   openpgp.KeyRing
-	changes   *DebChanges
+	SessionId  string // Name of the session
+	dir        string // Temporary directory for storage
+	keyRing    openpgp.KeyRing
+	requireSig bool
+	changes    *DebChanges
+}
+
+func (a *AptServer) NewUploadSession(sessionId string) *uploadSession {
+	var s uploadSession
+	s.SessionId = sessionId
+	s.keyRing = a.pubRing
+	s.dir = a.TmpDir + "/" + sessionId
+
+	os.Mkdir(s.dir, os.FileMode(0755))
+
+	a.sessMap.Set(sessionId, &s)
+	go pathHandle(a.sessMap, sessionId, a.TTL)
+
+	return &s
 }
 
 func (s *uploadSession) HandleReq(w http.ResponseWriter, r *http.Request) {
@@ -47,20 +58,6 @@ func (s *uploadSession) Close() {
 	os.Remove(s.dir)
 }
 
-func (a *AptServer) NewUploadSession(sessionId string) *uploadSession {
-	var s uploadSession
-	s.SessionId = sessionId
-	s.keyRing = a.pubRing
-	s.dir = a.TmpDir + "/" + sessionId
-
-	os.Mkdir(s.dir, os.FileMode(0755))
-
-	a.sessMap.Set(sessionId, &s)
-	go pathHandle(a.sessMap, sessionId, a.TTL)
-
-	return &s
-}
-
 func pathHandle(sessMap *SafeMap, s string, timeout time.Duration) {
 	time.Sleep(timeout)
 	c := sessMap.Get(s)
@@ -79,30 +76,6 @@ func pathHandle(sessMap *SafeMap, s string, timeout time.Duration) {
 }
 
 func (s *uploadSession) AddChanges(f multipart.File) (err error) {
-	b, err := ioutil.ReadAll(f)
-	if err != nil {
-		return
-	}
-
-	msg, rest := clearsign.Decode(b)
-	if len(rest) != 0 {
-		err = fmt.Errorf("changes file not signed")
-		return
-	}
-
-	br := bytes.NewReader(msg.Bytes)
-	signer, err := openpgp.CheckDetachedSignature(s.keyRing, br, msg.ArmoredSignature.Body)
-	if err != nil {
-		return
-	}
-
-	log.Println(signer)
-
-	br = bytes.NewReader(msg.Plaintext)
-	s.changes, err = ParseDebianChanges(br)
-	if err != nil {
-		return
-	}
-
+	s.changes, err = ParseDebianChanges(f, &s.keyRing)
 	return
 }
