04275f0b86b6c83d26f9b49f192004585a66bb80
Tristan Colgate <tristan@we7.com>
2014-07-14 08:02:01 +0100

    Remove failing non-test

--- aptserver_test.go
+++ aptserver_test.go
@@ -1,19 +1,2 @@
 package main
 
-var test_server1 = &AptServer{
-	MaxReqs:         2,
-	RepoBase:        "tests/repo",
-	PoolBase:        "tests/repo/pool",
-	TmpDir:          "tests/tmp",
-	CookieName:      "testrepo-cookie",
-	TTL:             3,
-	ValidateChanges: true,
-	ValidateDebs:    true,
-	AftpPath:        "tests/apt-ftpserver",
-	AftpConfig:      "tests/apt.cfg",
-	ReleaseConfig:   "tests/release.cfg",
-	PoolPattern:     nil,
-	PubRing:         nil,
-	PrivRing:        nil,
-	SignerId:        nil,
-}
