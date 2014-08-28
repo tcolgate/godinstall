641f313ca82f165591dddb0ddd2cda8ca0e71663
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2014-08-28 13:33:03 +0100

    Tests for blob store

--- store.go
+++ store.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/sha1"
+	"encoding/hex"
 	"errors"
 	"hash"
 	"io"
@@ -10,14 +11,18 @@ import (
 
 // An interface to a content addressable file store
 
-type StoreID string
+type StoreID []byte
+
+func (i StoreID) String() string {
+	return hex.EncodeToString(i)
+}
 
 type Storer interface {
 	Store() (StoreWriteCloser, error) // Write something to the store
 	Open(StoreID) (io.Reader, error)  // Open a file by id
 	Link(StoreID, ...string) error    // Link a file id to a given location
 	Delete(StoreID) error             // Delete an file by id
-	GarbageCollect()                  // Remove all files with no external links
+	GarbageCollect(chan<- struct{})   // Remove all files with no external links
 }
 
 type sha1Store struct {
@@ -35,8 +40,8 @@ func (t *sha1Store) Store() (StoreWriteCloser, error) {
 
 	mwriter := io.MultiWriter(file, h)
 
-	writer := &sha1StoreWriter{
-		sha1er: h,
+	writer := &hashedStoreWriter{
+		hasher: h,
 		writer: mwriter,
 	}
 	return writer, nil
@@ -54,7 +59,11 @@ func (t *sha1Store) Delete(id StoreID) error {
 	return nil
 }
 
-func (t *sha1Store) GarbageCollect() {
+func (t *sha1Store) GarbageCollect(done chan<- struct{}) {
+	go func() {
+		msg := new(struct{})
+		done <- *msg
+	}()
 	return
 }
 
@@ -72,29 +81,38 @@ func Sha1Store(baseDir string, tempDir string) Storer {
 // Write data, and call Close() when done. After calling Close,
 // Identitiy will return the ID of the item in the store.
 type StoreWriteCloser interface {
-	io.Writer
-	io.Closer
-	Identity() (StoreID, error) // Return the
+	io.WriteCloser
+	CloseAndLink(string) (StoreID, error) // Return the
+	Identity() (StoreID, error)           // Return the
 }
 
-type sha1StoreWriter struct {
+type hashedStoreWriter struct {
 	writer io.Writer
-	sha1er hash.Hash
+	hasher hash.Hash
 	closed bool
 }
 
-func (t *sha1StoreWriter) Write(p []byte) (n int, err error) {
-	return
-}
-
-func (t *sha1StoreWriter) Close() (err error) {
-	return
-}
-
-func (t *sha1StoreWriter) Identity() (id StoreID, err error) {
-	if !t.closed {
-		return "", errors.New("Identitty called before file storage was compete")
+func (w *hashedStoreWriter) Write(p []byte) (n int, err error) {
+	if w.closed {
+		return 0, errors.New("Attempt to write to closed file")
 	}
 
-	return StoreID(t.sha1er.Sum(nil)), nil
+	return w.writer.Write(p)
+}
+
+func (w *hashedStoreWriter) Close() (err error) {
+	w.closed = true
+	return
+}
+
+func (w *hashedStoreWriter) CloseAndLink(target string) (id StoreID, err error) {
+	return
+}
+
+func (w *hashedStoreWriter) Identity() (id StoreID, err error) {
+	if !w.closed {
+		return nil, errors.New("Identitty called before file storage was compete")
+	}
+
+	return StoreID(w.hasher.Sum(nil)), nil
 }
--- store_test.go
+++ store_test.go
@@ -1,7 +1,128 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func makeTestSha1Store(t *testing.T) (Storer, func(), error) {
+	testTempDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Errorf("Test setup failed, %v", err)
+		return nil, nil, err
+	}
+
+	testBaseDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		os.RemoveAll(testTempDir)
+		t.Errorf("Test setup failed, %v", err)
+		return nil, nil, err
+	}
+
+	clean := func() {
+		os.RemoveAll(testTempDir)
+		os.RemoveAll(testBaseDir)
+	}
+
+	return Sha1Store(testBaseDir, testTempDir), clean, nil
+}
 
 func TestSha1Store(t *testing.T) {
-	store := Sha1Store()
+	_, clean, _ := makeTestSha1Store(t)
+	defer clean()
+}
+
+func TestStore(t *testing.T) {
+	s, clean, _ := makeTestSha1Store(t)
+	defer clean()
+
+	writer, err := s.Store()
+
+	if err != nil {
+		t.Errorf("Call to store failed, %v", err)
+	}
+
+	writer.Write([]byte("Store some test info"))
+	if err != nil {
+		t.Errorf("Call to Write failed, %v", err)
+	}
+
+	err = writer.Close()
+	if err != nil {
+		t.Errorf("Call to Close failed, %v", err)
+	}
+
+	id, err := writer.Identity()
+	if err != nil {
+		t.Errorf("Call to Close failed, %v", err)
+	}
+	t.Log("ID: " + id.String())
+}
+
+func TestWriteAfterClose(t *testing.T) {
+	s, clean, _ := makeTestSha1Store(t)
+	defer clean()
+
+	writer, err := s.Store()
+
+	if err != nil {
+		t.Errorf("Call to store failed, %v", err)
+	}
+
+	n, err := writer.Write([]byte("Store some test info"))
+	if err != nil {
+		t.Errorf("Call to Write failed, %v", err)
+	}
+
+	err = writer.Close()
+	if err != nil {
+		t.Errorf("Call to Close failed, %v", err)
+	}
+
+	n, err = writer.Write([]byte("Store some test info"))
+	if err == nil {
+		t.Errorf("Call to Write after Close did not fail, returned %v", n)
+	}
+}
+
+func TestPrematureIdentity(t *testing.T) {
+	s, clean, _ := makeTestSha1Store(t)
+	defer clean()
+
+	writer, err := s.Store()
+
+	if err != nil {
+		t.Errorf("Call to store failed, %v", err)
+	}
+
+	writer.Write([]byte("Store some test info"))
+	if err != nil {
+		t.Errorf("Call to Write failed, %v", err)
+	}
+
+	id, err := writer.Identity()
+	if err == nil {
+		t.Errorf("Call to Identity before Close did not fail, %v", id)
+	}
+}
+
+func TestGarbageCollect1(t *testing.T) {
+	s, clean, _ := makeTestSha1Store(t)
+	defer clean()
+
+	s.GarbageCollect(nil)
+	return
+}
+
+func TestGarbageCollect2(t *testing.T) {
+	s, clean, _ := makeTestSha1Store(t)
+	defer clean()
+
+	done := make(chan struct{})
+
+	s.GarbageCollect(done)
+	<-done
+
+	return
 }
