c472391d3e6f92129bf3461d31efbfe25216d502
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2015-01-03 11:29:40 +0000

    Move dists handlers over to using read/write locking

--- dists_handlers.go
+++ dists_handlers.go
@@ -11,106 +11,118 @@ import (
 func httpDistsHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		vars := mux.Vars(r)
-		name, nameGiven := vars["name"]
-		dists := state.Archive.Dists()
-		if !nameGiven {
-			SendOKResponse(w, dists)
-		} else {
-			_, ok := dists[name]
-			if !ok {
-				http.NotFound(w, r)
-				return
-			}
-			rel, err := state.Archive.GetDist(name)
-			SendOKOrErrorResponse(w, rel, err, http.StatusInternalServerError)
-		}
+		handleWithReadLock(doHttpDistsGetHandler, w, r)
 	case "PUT":
-		vars := mux.Vars(r)
-		name, _ := vars["name"]
-		dists := state.Archive.Dists()
-		var rel *Release
-		var err error
-
-		_, exists := dists[name]
-		if exists {
-			http.Error(w,
-				"cannot update release directly",
-				http.StatusConflict)
-		} else {
-			seedrel := Release{
-				Suite: name,
-			}
-			rootid, err := state.Archive.GetReleaseRoot(seedrel)
-			if err != nil {
-				http.Error(w,
-					"Get reelase root failed"+err.Error(),
-					http.StatusInternalServerError)
-				return
-			}
-
-			emptyidx, err := state.Archive.EmptyReleaseIndex()
-			if err != nil {
-				http.Error(w,
-					"Get rempty release index failed"+err.Error(),
-					http.StatusInternalServerError)
-				return
-			}
-			newrelid, err := NewRelease(state.Archive, rootid, emptyidx, []ReleaseLogAction{})
-			if err != nil {
-				http.Error(w,
-					"Create new release failed, "+err.Error(),
-					http.StatusInternalServerError)
-				return
-			}
-			err = state.Archive.SetDist(name, newrelid)
-			if err != nil {
-				http.Error(w,
-					"Setting new dist tag failed, "+err.Error(),
-					http.StatusInternalServerError)
-				return
-			}
-			rel, err = state.Archive.GetDist(name)
-			if err != nil {
-				http.Error(w,
-					"retrieving new dist tag failed, "+err.Error(),
-					http.StatusInternalServerError)
-				return
-			}
-		}
-
-		output, err := json.Marshal(rel)
-		if err != nil {
-			http.Error(w,
-				"failed to retrieve distribution details, "+err.Error(),
-				http.StatusInternalServerError)
-		}
-		w.Write(output)
+		handleWithWriteLock(doHttpDistsPutHandler, w, r)
 	case "DELETE":
-		vars := mux.Vars(r)
-		name, nameGiven := vars["name"]
-		dists := state.Archive.Dists()
-		if !nameGiven {
-			http.Error(w,
-				http.StatusText(http.StatusMethodNotAllowed),
-				http.StatusMethodNotAllowed)
-		} else {
-			_, ok := dists[name]
-			if !ok {
-				http.NotFound(w, r)
-				return
-			}
-			err := state.Archive.DeleteDist(name)
-			if err != nil {
-				http.Error(w,
-					"failed to retrieve distribution details, "+err.Error(),
-					http.StatusInternalServerError)
-			}
-		}
+		handleWithWriteLock(doHttpDistsDeleteHandler, w, r)
 	default:
 		http.Error(w,
 			http.StatusText(http.StatusMethodNotAllowed),
 			http.StatusMethodNotAllowed)
 	}
 	return
+}
+
+func doHttpDistsGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name, nameGiven := vars["name"]
+	dists := state.Archive.Dists()
+	if !nameGiven {
+		SendOKResponse(w, dists)
+	} else {
+		_, ok := dists[name]
+		if !ok {
+			http.NotFound(w, r)
+			return
+		}
+		rel, err := state.Archive.GetDist(name)
+		SendOKOrErrorResponse(w, rel, err, http.StatusInternalServerError)
+	}
+}
+
+func doHttpDistsPutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name, _ := vars["name"]
+	dists := state.Archive.Dists()
+	var rel *Release
+	var err error
+
+	_, exists := dists[name]
+	if exists {
+		http.Error(w,
+			"cannot update release directly",
+			http.StatusConflict)
+	} else {
+		seedrel := Release{
+			Suite: name,
+		}
+		rootid, err := state.Archive.GetReleaseRoot(seedrel)
+		if err != nil {
+			http.Error(w,
+				"Get reelase root failed"+err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+
+		emptyidx, err := state.Archive.EmptyReleaseIndex()
+		if err != nil {
+			http.Error(w,
+				"Get rempty release index failed"+err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+		newrelid, err := NewRelease(state.Archive, rootid, emptyidx, []ReleaseLogAction{})
+		if err != nil {
+			http.Error(w,
+				"Create new release failed, "+err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+		err = state.Archive.SetDist(name, newrelid)
+		if err != nil {
+			http.Error(w,
+				"Setting new dist tag failed, "+err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+		rel, err = state.Archive.GetDist(name)
+		if err != nil {
+			http.Error(w,
+				"retrieving new dist tag failed, "+err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+	}
+
+	output, err := json.Marshal(rel)
+	if err != nil {
+		http.Error(w,
+			"failed to retrieve distribution details, "+err.Error(),
+			http.StatusInternalServerError)
+	}
+	w.Write(output)
+}
+
+func doHttpDistsDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name, nameGiven := vars["name"]
+	dists := state.Archive.Dists()
+	if !nameGiven {
+		http.Error(w,
+			http.StatusText(http.StatusMethodNotAllowed),
+			http.StatusMethodNotAllowed)
+	} else {
+		_, ok := dists[name]
+		if !ok {
+			http.NotFound(w, r)
+			return
+		}
+		err := state.Archive.DeleteDist(name)
+		if err != nil {
+			http.Error(w,
+				"failed to retrieve distribution details, "+err.Error(),
+				http.StatusInternalServerError)
+		}
+	}
 }
