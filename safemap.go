786228ce16e55035ffd90e82e2c17db3fa5e3876
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2015-08-27 20:22:20 +0100

    Use an embedded mutex, less stutter

--- safemap.go
+++ safemap.go
@@ -6,22 +6,22 @@ import (
 
 // SafeMap is a thread safe map stolen from somwhere else
 type SafeMap struct {
-	lock *sync.RWMutex
-	bm   map[interface{}]interface{}
+	sync.RWMutex
+	bm map[interface{}]interface{}
 }
 
 // NewSafeMap creates a thread safe map
 func NewSafeMap() *SafeMap {
 	return &SafeMap{
-		lock: new(sync.RWMutex),
-		bm:   make(map[interface{}]interface{}),
+		RWMutex: sync.RWMutex{},
+		bm:      make(map[interface{}]interface{}),
 	}
 }
 
 //Get from maps return the k's value
 func (m *SafeMap) Get(k interface{}) interface{} {
-	m.lock.RLock()
-	defer m.lock.RUnlock()
+	m.RLock()
+	defer m.RUnlock()
 	if val, ok := m.bm[k]; ok {
 		return val
 	}
@@ -31,8 +31,8 @@ func (m *SafeMap) Get(k interface{}) interface{} {
 // Set maps the given key and value. Returns false
 // if the key is already in the map and changes nothing.
 func (m *SafeMap) Set(k interface{}, v interface{}) bool {
-	m.lock.Lock()
-	defer m.lock.Unlock()
+	m.Lock()
+	defer m.Unlock()
 	if val, ok := m.bm[k]; !ok {
 		m.bm[k] = v
 	} else if val != v {
@@ -45,8 +45,8 @@ func (m *SafeMap) Set(k interface{}, v interface{}) bool {
 
 // Check returns true if k is exist in the map.
 func (m *SafeMap) Check(k interface{}) bool {
-	m.lock.RLock()
-	defer m.lock.RUnlock()
+	m.RLock()
+	defer m.RUnlock()
 	if _, ok := m.bm[k]; !ok {
 		return false
 	}
@@ -55,7 +55,7 @@ func (m *SafeMap) Check(k interface{}) bool {
 
 // Delete removes a key from a map
 func (m *SafeMap) Delete(k interface{}) {
-	m.lock.Lock()
-	defer m.lock.Unlock()
+	m.Lock()
+	defer m.Unlock()
 	delete(m.bm, k)
 }
